e64c15e88954404c5989771c44843f6b7c2aea5f
Tom.Li <open222333@gmail.com>
2023-12-01 19:24:27 +0800

    20231201

--- src/tool.py
+++ src/tool.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from .logger import Log
 from . import LOG_LEVEL
 import os
@@ -11,4 +10,29 @@ if LOG_LEVEL:
 logger.set_date_handler()
 logger.set_msg_handler()
 
+
+def get_all_files(dir_path, extensions=None):
+    '''取得所有檔案
+    dir_path: 檔案資料夾
+    extensions: 指定副檔名,若無指定則全部列出
+    '''
+    target_file_path = []
+    path = os.path.abspath(dir_path)
+
+    for file in os.listdir(path):
+        _, file_extension = os.path.splitext(file)
+        if extensions:
+            allow_extension = [f'.{e}' for e in extensions]
+            if file_extension in allow_extension:
+                target_file_path.append(f'{dir_path}/{file}')
+        else:
+            target_file_path.append(f'{dir_path}/{file}')
+
+        # 遞迴
+        if os.path.isdir(f'{dir_path}/{file}'):
+            files = get_all_files(f'{dir_path}/{file}', extensions)
+            for file in files:
+                target_file_path.append(file)
+    target_file_path.sort()
+    return target_file_path
 
