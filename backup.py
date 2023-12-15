0c29c5f1b366c493d81dfa704c8522160d6afe2b
Tom.Li <open222333@gmail.com>
2023-12-15 16:38:50 +0800

    修改 只在最外層確認是否執行

--- backup.py
+++ backup.py
@@ -21,100 +21,97 @@ if __name__ == "__main__":
 
             # 執行匯出
             if info['action'].get('dump'):
-                if info['action']['dump'].get('execute'):
 
-                    host = info['action']['dump'].get('host')
-                    if host == None:
-                        host = '127.0.0.1'
+                host = info['action']['dump'].get('host')
+                if host == None:
+                    host = '127.0.0.1'
 
-                    port = info['action']['dump'].get('port')
-                    if port == None:
-                        port = '27017'
+                port = info['action']['dump'].get('port')
+                if port == None:
+                    port = '27017'
 
-                    username = info['action']['dump'].get('username')
-                    password = info['action']['dump'].get('password')
+                username = info['action']['dump'].get('username')
+                password = info['action']['dump'].get('password')
 
-                    hostname = info['action']['dump'].get('hostname')
-                    if hostname == None:
-                        hostname = socket.gethostname()
+                hostname = info['action']['dump'].get('hostname')
+                if hostname == None:
+                    hostname = socket.gethostname()
 
-                    for item in info['action']['dump']['items']:
-                        database = item['database']
-                        collections = item.get('collections', [])
+                for item in info['action']['dump']['items']:
+                    database = item['database']
+                    collections = item.get('collections', [])
 
-                        # 若沒指定 collections 則預設全部
-                        if len(collections) == 0:
-                            mmc = MongoMappingCollections(f'{host}:{port}')
-                            mmc.set_databases(database)
-                            collections = mmc.get_all_collections()[database]
+                    # 若沒指定 collections 則預設全部
+                    if len(collections) == 0:
+                        mmc = MongoMappingCollections(f'{host}:{port}')
+                        mmc.set_databases(database)
+                        collections = mmc.get_all_collections()[database]
 
-                        for collection in collections:
-                            mt = MongoTool(
-                                host=f'{host}:{port}',
-                                database=database,
-                                collection=collection,
-                                dir_path=os.path.join(OUTPUT_DIR, hostname)
-                            )
-                            if username and password:
-                                mt.set_auth(username, password)
-                            mt.dump()
+                    for collection in collections:
+                        mt = MongoTool(
+                            host=f'{host}:{port}',
+                            database=database,
+                            collection=collection,
+                            dir_path=os.path.join(OUTPUT_DIR, hostname)
+                        )
+                        if username and password:
+                            mt.set_auth(username, password)
+                        mt.dump()
 
             # 執行匯入
             if info['action'].get('restore'):
-                if info['action']['restore'].get('execute'):
+                host = info['action']['restore'].get('host')
+                if host == None:
+                    host = '127.0.0.1'
 
-                    host = info['action']['restore'].get('host')
-                    if host == None:
-                        host = '127.0.0.1'
+                port = info['action']['restore'].get('port')
+                if port == None:
+                    port = '27017'
 
-                    port = info['action']['restore'].get('port')
-                    if port == None:
-                        port = '27017'
+                username = info['action']['restore'].get('username')
+                password = info['action']['restore'].get('password')
 
-                    username = info['action']['restore'].get('username')
-                    password = info['action']['restore'].get('password')
+                hostname = info['action']['restore'].get('hostname')
+                if hostname == None:
+                    hostname = socket.gethostname()
 
-                    hostname = info['action']['restore'].get('hostname')
-                    if hostname == None:
-                        hostname = socket.gethostname()
+                for item in info['action']['restore']['items']:
+                    database = item['database']
+                    collections = item.get('collections', [])
 
-                    for item in info['action']['restore']['items']:
-                        database = item['database']
-                        collections = item.get('collections', [])
+                    if len(collections) == 0:
+                        if item.get('dirpath'):
+                            collections = parse_db_collections(item.get('dirpath'))[database]
+                        else:
+                            continue
 
-                        if len(collections) == 0:
-                            if item.get('dirpath'):
-                                collections = parse_db_collections(item.get('dirpath'))[database]
-                            else:
-                                continue
+                    for collection in collections:
+                        mt = MongoTool(
+                            host=f'{host}:{port}',
+                            database=database,
+                            collection=collection,
+                            dir_path=os.path.join(OUTPUT_DIR, hostname)
+                        )
 
-                        for collection in collections:
-                            mt = MongoTool(
-                                host=f'{host}:{port}',
-                                database=database,
-                                collection=collection,
-                                dir_path=os.path.join(OUTPUT_DIR, hostname)
-                            )
+                        if username and password:
+                            mt.set_auth(username, password)
 
-                            if username and password:
-                                mt.set_auth(username, password)
+                        # 刪除目前的集合
+                        if info['action']['restore'].get('drop_collection'):
+                            mt.drop_collection()
 
-                            # 刪除目前的集合
-                            if info['action']['restore'].get('drop_collection'):
-                                mt.drop_collection()
+                        # 指定日期
+                        if info['action']['restore'].get('date'):
+                            mt.set_date(date=info['action']['restore']['date'])
 
-                            # 指定日期
-                            if info['action']['restore'].get('date'):
-                                mt.set_date(date=info['action']['restore']['date'])
+                        # 集合名稱不帶日期
+                        if info['action']['restore'].get('attach_date'):
+                            mt.restore(name=f'{collection}_{mt.date}')
+                        else:
+                            mt.restore()
 
-                            # 集合名稱不帶日期
-                            if info['action']['restore'].get('attach_date'):
-                                mt.restore(name=f'{collection}_{mt.date}')
-                            else:
-                                mt.restore()
-
-                        # 清空集合資料
-                        if info['action']['restore'].get('clear_doc'):
-                            if len(collections) != 0:
-                                for collection in collections:
-                                    mt.delete_all_document()
+                    # 清空集合資料
+                    if info['action']['restore'].get('clear_doc'):
+                        if len(collections) != 0:
+                            for collection in collections:
+                                mt.delete_all_document()
